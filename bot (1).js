ddaca9973227354af75e6fd3bf84d789132048cd
xxmoussaabxx <45010213+xxmoussaabxx@users.noreply.github.com>
2018-11-13 18:50:30 +0100

    Update bot (1).js

--- bot (1).js
+++ bot (1).js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
+client.user.setGame(`ops`,"http://twitch.tv/ops")
 client.user.setStatus("dnd")
 });
 
